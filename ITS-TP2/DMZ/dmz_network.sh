83f7f2461db375a4ddb75240bf32908836855790
Dylan Perdigão <dylanperdigao@icloud.com>
2022-04-22 23:16:09 +0100

    coisas da vida...

--- ITS-TP2/DMZ/dmz_network.sh
+++ ITS-TP2/DMZ/dmz_network.sh
@@ -22,7 +22,7 @@ iface ens256 inet static
     broadcast 10.10.10.255
 
 up ip route add 10.20.20.0/24 via 10.10.10.1 dev ens256
-up ip route add 0/0 via 10.10.10.1 dev ens256
+up ip route add add 192.168.93.0/24 via 10.10.10.1 dev ens256
 """ > /etc/network/interfaces
 sudo systemctl restart networking
 sudo dhclient
--- ITS-TP2/Internal/in_network.sh
+++ ITS-TP2/Internal/in_network.sh
@@ -10,7 +10,7 @@ iface lo inet loopback
 
 auto ens160
 iface ens160 inet dhcp
-    address 192.168.93.132
+    address 192.168.93.163
     netmask 255.255.255.0
 
 auto ens256
@@ -25,4 +25,3 @@ up ip route add 10.10.10.0/24 via 10.20.20.1 dev ens256
 up ip route add 0/0 via 10.20.20.1 dev ens256
 """ > /etc/network/interfaces
 sudo systemctl restart networking
-sudo dhclient
